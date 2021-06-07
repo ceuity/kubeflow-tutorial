b647f4dcc6dfeb81225fb04e2223cd0498b7e610
ceuity <everland7942@gmail.com>
2021-06-08 02:39:58 +0900

    Add: kfctl setup script

---
+++ setup/kfctl_setup_linux.sh
@@ -0,0 +1,5 @@
+wget https://github.com/kubeflow/kfctl/releases/download/v1.2.0/kfctl_v1.2.0-0-gbc038f9_linux.tar.gz
+tar -xvf kfctl_v1.2.0-0-gbc038f9_linux.tar.gz
+sudo mv kfctl /usr/local/bin/
+rm kfctl_v1.2.0-0-gbc038f9_linux.tar.gz
+kfctl version
